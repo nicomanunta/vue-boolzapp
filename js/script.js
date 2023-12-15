a34eb7d2ed42e750b5559819d46b065d0348c48d
nicomanunta <nic.manun@gmail.com>
2023-12-15 15:04:48 +0100

    problema milestone 4

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let { createApp } = Vue
 createApp({
     data(){
         return{
+            search: "",
             mess: "",
             newMessage: "",
             activeChat: 0,
@@ -14,16 +15,19 @@ createApp({
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
+                            time: date.split(""),
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
+                            time: date.split(""),
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 16:15:22',
+                            time: date.split(""),
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -36,16 +40,19 @@ createApp({
                     messages: [
                         {
                             date: '20/03/2020 16:30:00',
+                            time: date.split(""),
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
                             date: '20/03/2020 16:30:55',
+                            time: date.split(""),
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
                             date: '20/03/2020 16:35:00',
+                            time: date.split(""),
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -58,16 +65,19 @@ createApp({
                     messages: [
                         {
                             date: '28/03/2020 10:10:40',
+                            time: date.split(""),
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
                             date: '28/03/2020 10:20:10',
+                            time: date.split(""),
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
                             date: '28/03/2020 16:15:22',
+                            time: date.split(""),
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -80,11 +90,13 @@ createApp({
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
+                            time: date.split(""),
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
+                            time: date.split(""),
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -97,11 +109,13 @@ createApp({
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
+                            time: date.split(""),
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
+                            time: date.split(""),
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -114,16 +128,19 @@ createApp({
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
+                            time: date.split(""),
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
+                            time: date.split(""),
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
                             date: '10/01/2020 15:51:00',
+                            time: date.split(""),
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -136,11 +153,13 @@ createApp({
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
+                            time: date.split(""),
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
+                            time: date.split(""),
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -153,16 +172,19 @@ createApp({
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
+                            time: date.split(""),
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
                             date: '10/01/2020 15:50:00',
+                            time: date.split(""),
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:51:00',
+                            time: date.split(""),
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -171,6 +193,7 @@ createApp({
             ]
 
         }
+        
     },
     methods:{
         selectChat(index){
@@ -193,7 +216,21 @@ createApp({
                     status: 'received' 
                 }) 
             },1000)  
-        }
+        },
+        searchChat(){
+            this.contacts.forEach(element => {
+                if(element.name.toLowerCase().includes(this.search.toLowerCase())){
+                    element.visible = true
+                }
+                else{
+                    element.visible = false 
+                    console.log(element.name)
+                }
+            });
+        },
+       
+        
+        
         
     }
 }).mount("#app")
